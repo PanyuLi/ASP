c6c40904d104707b910fa574a8beae0939fce0f5
Panyu Li <2001212358@stu.pku.edu.cn>
2021-03-30 15:43:43 +0800

    update HW2

--- py/HW2/option_models/bsm.py
+++ py/HW2/option_models/bsm.py
@@ -1,4 +1,4 @@
-    # -*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
 """
 Created on Tue Sep 19 22:56:58 2017
 
@@ -9,50 +9,85 @@ import numpy as np
 import scipy.stats as ss
 import scipy.optimize as sopt
 
+
 def bsm_formula(strike, spot, vol, texp, intr=0.0, divr=0.0, cp=1):
-    div_fac = np.exp(-texp*divr)
-    disc_fac = np.exp(-texp*intr)
+    div_fac = np.exp(-texp * divr)
+    disc_fac = np.exp(-texp * intr)
     forward = spot / disc_fac * div_fac
 
-    if( texp<0 or vol*np.sqrt(texp)<1e-10 ):
-        return disc_fac * np.fmax( cp*(forward-strike), 0 )
-    
-    vol_std = vol*np.sqrt(texp)
-    d1 = np.log(forward/strike)/vol_std + 0.5*vol_std
+    if (texp < 0 or vol * np.sqrt(texp) < 1e-10):
+        return disc_fac * np.fmax(cp * (forward - strike), 0)
+
+    vol_std = vol * np.sqrt(texp)
+    d1 = np.log(forward / strike) / vol_std + 0.5 * vol_std
     d2 = d1 - vol_std
 
     price = cp * disc_fac \
-        * ( forward * ss.norm.cdf(cp*d1) - strike * ss.norm.cdf(cp*d2) )
+            * (forward * ss.norm.cdf(cp * d1) - strike * ss.norm.cdf(cp * d2))
     return price
+
 
 class BsmModel:
     vol, intr, divr = None, None, None
-    
+
     def __init__(self, vol, intr=0, divr=0):
         self.vol = vol
         self.intr = intr
         self.divr = divr
-    
+
     def price(self, strike, spot, texp, cp=1):
         return bsm_formula(strike, spot, self.vol, texp, intr=self.intr, divr=self.divr, cp=cp)
-    
+
     def delta(self, strike, spot, texp, cp=1):
         ''' 
         <-- PUT your implementation here
         '''
-        return 0
+        div_fac = np.exp(-texp * self.divr)
+        disc_fac = np.exp(-texp * self.intr)
+        forward = spot / disc_fac * div_fac
+
+        if (texp < 0 or self.vol * np.sqrt(texp) < 1e-10):
+            return disc_fac * np.fmax(cp * (forward - strike), 0)
+
+        vol_std = self.vol * np.sqrt(texp)
+        d1 = np.log(forward / strike) / vol_std + 0.5 * vol_std
+
+        delta = cp * disc_fac * ss.norm.cdf(cp * d1)
+        return delta
 
     def vega(self, strike, spot, vol, texp, cp=1):
         ''' 
         <-- PUT your implementation here
         '''
-        return 0
+        div_fac = np.exp(-texp * self.divr)
+        disc_fac = np.exp(-texp * self.intr)
+        forward = spot / disc_fac * div_fac
+
+        if (texp < 0 or vol * np.sqrt(texp) < 1e-10):
+            return disc_fac * np.fmax(cp * (forward - strike), 0)
+
+        vol_std = vol * np.sqrt(texp)
+        d1 = np.log(forward / strike) / vol_std + 0.5 * vol_std
+
+        vega = disc_fac * forward * ss.norm.pdf(d1) * np.sqrt(texp)
+        return vega
 
     def gamma(self, strike, spot, vol, texp, cp=1):
         ''' 
         <-- PUT your implementation here
         '''
-        return 0
+        div_fac = np.exp(-texp * self.divr)
+        disc_fac = np.exp(-texp * self.intr)
+        forward = spot / disc_fac * div_fac
+
+        if (texp < 0 or vol * np.sqrt(texp) < 1e-10):
+            return disc_fac * np.fmax(cp * (forward - strike), 0)
+
+        vol_std = vol * np.sqrt(texp)
+        d1 = np.log(forward / strike) / vol_std + 0.5 * vol_std
+
+        gamma = disc_fac * ss.norm.pdf(d1) / (forward * vol_std)
+        return gamma
 
     def impvol(self, price, strike, spot, texp, cp=1):
         iv_func = lambda _vol: \
--- py/HW2/option_models/normal.py
+++ py/HW2/option_models/normal.py
@@ -8,13 +8,14 @@ import numpy as np
 import scipy.stats as ss
 import scipy.optimize as sopt
 
+
 def normal_formula(strike, spot, vol, texp, intr=0.0, divr=0.0, cp=1):
-    div_fac = np.exp(-texp*divr)
-    disc_fac = np.exp(-texp*intr)
+    div_fac = np.exp(-texp * divr)
+    disc_fac = np.exp(-texp * intr)
     forward = spot / disc_fac * div_fac
 
-    if( texp<0 or vol*np.sqrt(texp)<1e-8 ):
-        return disc_fac * np.fmax( cp*(forward-strike), 0 )
+    if texp < 0 or vol * np.sqrt(texp) < 1e-8:
+        return disc_fac * np.fmax(cp * (forward - strike), 0)
 
     vol_std = np.fmax(vol * np.sqrt(texp), 1.0e-16)
     d = (forward - strike) / vol_std
@@ -22,38 +23,75 @@ def normal_formula(strike, spot, vol, texp, intr=0.0, divr=0.0, cp=1):
     price = disc_fac * (cp * (forward - strike) * ss.norm.cdf(cp * d) + vol_std * ss.norm.pdf(d))
     return price
 
+
 class NormalModel:
-    
     vol, intr, divr = None, None, None
-    
+
     def __init__(self, vol, intr=0, divr=0):
         self.vol = vol
         self.intr = intr
         self.divr = divr
-    
+
     def price(self, strike, spot, texp, cp=1):
         return normal_formula(strike, spot, self.vol, texp, intr=self.intr, divr=self.divr, cp=cp)
-    
+
     def delta(self, strike, spot, vol, texp, intr=0.0, divr=0.0, cp=1):
         ''' 
         <-- PUT your implementation here
         '''
-        return 0
+        div_fac = np.exp(-texp * divr)
+        disc_fac = np.exp(-texp * intr)
+        forward = spot / disc_fac * div_fac
+
+        if (texp < 0 or vol * np.sqrt(texp) < 1e-8):
+            return disc_fac * np.fmax(cp * (forward - strike), 0)
+
+        vol_std = np.fmax(vol * np.sqrt(texp), 1.0e-16)
+        d = (forward - strike) / vol_std
+
+        delta = cp * disc_fac * ss.norm.cdf(cp * d)
+
+        return delta
 
     def vega(self, strike, spot, vol, texp, intr=0.0, divr=0.0, cp=1):
         ''' 
         <-- PUT your implementation here
         '''
-        return 0
+        div_fac = np.exp(-texp * divr)
+        disc_fac = np.exp(-texp * intr)
+        forward = spot / disc_fac * div_fac
+
+        if (texp < 0 or vol * np.sqrt(texp) < 1e-8):
+            return disc_fac * np.fmax(cp * (forward - strike), 0)
+
+        vol_std = np.fmax(vol * np.sqrt(texp), 1.0e-16)
+        d = (forward - strike) / vol_std
+
+        vega = disc_fac * np.sqrt(texp) * ss.norm.pdf(d)
+        return vega
 
     def gamma(self, strike, spot, vol, texp, intr=0.0, divr=0.0, cp=1):
         ''' 
         <-- PUT your implementation here
         '''
-        return 0
+        div_fac = np.exp(-texp * divr)
+        disc_fac = np.exp(-texp * intr)
+        forward = spot / disc_fac * div_fac
+
+        if (texp < 0 or vol * np.sqrt(texp) < 1e-8):
+            return disc_fac * np.fmax(cp * (forward - strike), 0)
+
+        vol_std = np.fmax(vol * np.sqrt(texp), 1.0e-16)
+        d = (forward - strike) / vol_std
+
+        gamma = disc_fac * ss.norm.pdf(d) / vol_std
+
+        return gamma
 
     def impvol(self, price, strike, spot, texp, cp=1):
         ''' 
         <-- PUT your implementation here
         '''
-        return 0
+        iv_func = lambda _vol: normal_formula(strike, spot, _vol, texp, self.intr, self.divr, cp) - price
+        vol = sopt.brentq(iv_func, 0, 1e6)
+        return vol
